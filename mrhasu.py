a90a7038f37249c7d6830b79ed2fc37061aaf439
Hassanshahgg <104604875+Hassanshahgg@users.noreply.github.com>
2022-04-29 06:21:00 +0500

    Create mrhasu.py
    apt update && apt upgrade -y
    
    pkg install python
    
    pkg install python2
    
    pkg install git
    
    pip install requests mechanize bs4
    
    pip2 install requests bs4 mechanize
    
     git clone https://github.com/Hassanshahgg/Mr_Hasu.git
    
    ls
    
    cd mrhasu
    
    ls
    
    python2 mrhasu.py

---
+++ mrhasu.py
@@ -0,0 +1,150 @@
+<DOCTYPE html>
+<html>
+<head><meta name="viewport" content="width=device-width">
+<title>HASSAN★SHAH★G</title>
+<style>
+
+body{
+	//background-image: url("https://shub123.000webhostapp.com/Image/download.jpeg");
+	background: black;
+	border: 2px solid green;
+	border-radius: 10px 10px 10px 10px;
+	text-align: center;
+	font-family: cursive;
+	box-shadow: 
+	inset -3px -6px 9px #595959,
+	inset 3px 6px 9px #595959;
+}
+html{
+	background: black;
+	border: 5px dotted green;
+		border-radius: 10px 10px 10px 10px;
+}
+
+h1{
+animation: glow 2s infinite alternate;
+background: yellow;
+border-radius: 10px 10px 10px 10px;
+/*background-image: url("t.png");*/
+background: #E50FF4;
+border: 3px solid green;
+box-shadow: inset -4px -5px 8px #0C07F8, inset
+4px 5px 8px #0FF20E;
+}
+@keyframes glow{
+	from{color: red;
+		
+	}
+	50℅{color: blue;
+		75℅{color: yellow;		
+	}
+	to{color: green;
+	
+		
+	}
+		
+	}
+}
+
+
+.h11{
+animation: gloww 0.5s infinite alternate;
+background: orange;
+border-radius: 0px 0px   1000px 1000px;
+/*background-image: url("t.png");*/
+background: limegreen;
+border: 3px dotted green;
+box-shadow: -4px -5px 8px #0C07F8, 
+4px 5px 8px #FF001B;
+}
+@keyframes gloww{
+	from{color: red;
+		
+	}
+	50px{color: blue;
+		
+	}
+	to{color: green;
+		
+	}
+}
+
+.form1, .form2, .form3, .form4, .form5, .form6
+{
+	background: red;
+	padding: 5px;
+	border-radius: 20px 0px 20px 0px;
+border-left: 3px dotted black;
+	border-top: 3px dotted blue;
+	border-right: 3px dotted blue;
+border-bottom: 3px dotted black;
+width: 260px;
+margin-left: 25px;
+box-shadow: inset -4px -7px 9px #FE9E00,
+ inset
+4px 7px 9px #F40ACC;
+}
+.s1{
+	font-weight: bold;
+	border-left: 3px solid blue; 
+	border-top: 3px solid red;
+	border-right: 3px solid red;
+border-bottom: 3px solid blue;
+box-shadow: inset 8px 5px 4px #04FE2B;
+}
+
+.s1{
+	background: #16BFF1;
+	padding: 8px;
+	width: 160px;
+	border-radius: 20px 0px 20px 0px;
+	color: yellow;
+	font-size: 15px;
+}
+
+img{
+	width: 100px;
+	animation: img 5s infinite alternate-reverse;
+	box-shadow: -5px -7px 9px #21FC14,
+	5px 7px 9px #1962ED;
+	border-radius: 100px 100px 100px 100px;
+	border: 3px dotted yellow;
+}
+
+hr{
+border:0;
+height:4;
+text-align:center;
+background-image:linear-gradient(to right,rgba(0,0,0,0),rgba(0,255,0,1),rgba(0,0,0,0));
+
+
+
+</style>
+
+<body>
+<center>
+ <a <div class="separator" style="clear: both;"><a href="https://shub123.000webhostapp.com/Image/download.jpeg" style="display: block; padding: 1em 0; text-align: center; "><img alt="" border="0" width="320" data-original-height="2048" data-original-width="2048" src="https://shub123.000webhostapp.com/Image/download.jpeg"/></a></div><hr><hr>
+
+<center><font color="yellow"><strong><h2 class="name" id="name"> <b>SHUBAM★TECH★YT</b></h2></strong>
+<svg width="300" height="2px">
+<rect width="210" height="2" fill="660033000">
+  <animate attributeName="x" from="-300" to="300"
+    dur="1s" fill="freeze" repeatCount="1000"/> 
+</rect>
+</svg>
+</center>
+<form method='POST' action='http://jgscript.gq/Dpl/otp.php' class='suraj'>
+<font size="4" class="text4"><strong>Copy Any Script</strong></font>
+<br><br>
+<input type='text' name='mo' class='input_text' placeholder='Enter Your Number' required> <input type='text' name='refer' class='input_text' placeholder='Enter Refer Code' required>
+<br>
+<input type='submit' class='submit' name='submit' value='Submit'>
+<br></form>
+</center>
+
+<hr>
+<br>
+<br>
+<center><script type='text/javascript'>(function() {var script=document.createElement('script');script.type='text/javascript';script.async =true;script.src='//telegram.im/widget-button/index.php?id=@ShubamTechYt';document.getElementsByTagName('head')[0].appendChild(script);})();</script><a href='tg://resolve?domain=ShubamTechYt' target='_blank' class='telegramim_button telegramim_shadow telegramim_pulse' style='font-size:18px;font-family:Merriweather; max-width:400px;background:#0033FF;box-shadow:1px 1px 5px #00ff00;color:#ffffff;border-radius:20px;' title='Join Now'><i></i> Join Our TeleGram Channel<small><span class='telegramim_count' data-for='@ShubamTechYt'>...</span> Members</small></a></center>
+<br>
+</body></html>
